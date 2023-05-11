99bccb19b07d02dd4ad9f4700a8f955e199cbeb3
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-05-10 21:37:47 -0300

    index.html e index.js
    Recursos nativos no navegador

---
+++ aula12/src/index.js
@@ -0,0 +1,11 @@
+let nome = "Gustavo";
+
+console.log(nome);
+console.error(nome); // alerta erro em vermelho
+console.warn(nome); //alerta em amarelo no console
+
+// window.alert(nome); //mensagem popup
+// nome = window.prompt("Insira o seu nome:");
+// let confirmacao = window.confirm("Confirma essa ação?");
+
+document; //faz referência a pg HTML
