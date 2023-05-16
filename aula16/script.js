a3a14c2ca5f7ae12a3b1d6f1d3dba3ce46fec794
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-05-16 20:09:49 -0300

    Estruturas condicionais If e Else

---
+++ aula16/script.js
@@ -0,0 +1,13 @@
+/* const idade = prompt("Informe sua idade:");
+
+if (idade > 18) {
+  alert("Você é mair de idade");
+} else if (idade > 12) {
+  alert("Você é menor de idade");
+} else if (idade > 4) {
+  alert("Você é criança");
+} else {
+  alert("..");
+}
+*/
+const = resultado = (6 === 6) ? "Verdadeiro" : "Falso"
