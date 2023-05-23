b120e713ebcc962ae17a7b0e33f6e22250f294c3
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-05-22 21:40:39 -0300

    ex004-visitando-cidades
    Exercício para treinamento da condição while e somatório de strings

---
+++ aula25-exercicio004/cidades.js
@@ -0,0 +1,22 @@
+const turista = prompt("E aí Turista! Qual seu nome?:");
+
+let cidades = "";
+let contagem = 0;
+
+let continuar = prompt("Você visitou alguma cidade?: (Sim/Não)");
+
+while (continuar === "Sim") {
+  let cidade = prompt("Qual cidade visitou?:");
+  cidades += "-" + cidade + "\n";
+  contagem++;
+  continuar = prompt("Você visitou alguma outra cidade?: (Sim/Não)");
+}
+
+alert(
+  "Turista: " +
+    turista +
+    "\nQuantidade de cidades visitadas: " +
+    contagem +
+    "\nCidades visitadas:\n" +
+    cidades
+);
