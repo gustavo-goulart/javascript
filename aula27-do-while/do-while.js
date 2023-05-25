172c034b5eeb7c6b064740ed92330a61b1695e5b
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-05-25 19:56:29 -0300

    do-while
    processo de como utilizar a estrutura de repetição do while

---
+++ aula27-do-while/do-while.js
@@ -0,0 +1,8 @@
+let velocidade = 0;
+
+do {
+  alert("A velocidade do veículo é " + velocidade + " km/h");
+  velocidade -= 20;
+} while (false);
+
+alert("Velocidade Final: " + velocidade + " km/h");
