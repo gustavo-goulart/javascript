6dbda7571dd648dd86516b999dde4e26f729e3a8
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-06-03 11:06:24 -0300

    estrutura-de-dados-array
    Conceitos e usabilidade de um array

---
+++ aula34-arrays/array.js
@@ -0,0 +1,11 @@
+const listaDeCompras = ["Açucar", false];
+console.log(listaDeCompras);
+
+listaDeCompras[0] = "Arroz";
+listaDeCompras[1] = "Feijão"; /* REFERÊNCIAR A ESTRUTURA ARRAY */
+listaDeCompras[2] = "Carne Moída";
+listaDeCompras[3] = 8;
+
+console.log(listaDeCompras);
+
+console.log(listaDeCompras[10]);
